ee069a95b066c69f63e6abd4f5f2f9f3716b098b
Aliyu <biggy0023@gmail.com>
2022-09-15 11:24:59 +0100

    My ruby eye sight has gotten a bit better. This feels a lot simpler

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,17 +1,30 @@
-def bubble_sort(arr)
-  
-  loop do
-   length = arr.length - 1
-    sorted = true
-    for i in 0..(length - 1)
-      if arr[i] > arr[i + 1]
-        arr[i + 1], arr[i] = arr[i], arr[i  + 1]
-        sorted = false
-      end
-    end
-    break if sorted == true
-  end
-  return arr
+# frozen_string_literal: false
+
+# build a method that takes in an array and returns a sorted array
+
+random_array = [10, 3, 2, 4, 10, 58, 67, 29, 85, 4, 0, 4, 5, 9]
+
+# method with normal sort
+def normal_sort(array)
+  array.sort
 end
 
-p bubble_sort([2,53,1,3,5])
+# bubble sort method
+def bubble_sort(array)
+  bubble_sorted = false
+
+  while bubble_sorted == false
+    index = 0
+    bubble_sorted = true
+    while index < (array.length - 1)
+      if array[index] > array[index + 1]
+        array[index], array[index + 1] = array[index + 1], array[index]
+        bubble_sorted = false # this is what makes it reiterate one more time, until it is eventually true
+      end
+      index += 1
+    end
+  end
+  array
+end
+
+p bubble_sort(random_array)
